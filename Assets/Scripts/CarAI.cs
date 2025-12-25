fa7c6bc9d47fba58e9111059da251c99910d6ce8
Juanquirr <juanquirr579@gmail.com>
2025-12-25 15:00:25 +0000

    CarEngine and CarAi scripts adjustment with new cars

--- Assets/Scripts/CarAI.cs
+++ Assets/Scripts/CarAI.cs
@@ -13,6 +13,6 @@ public class CarAI : MonoBehaviour
     
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
     }
 }
--- Assets/Scripts/CarEngine.cs
+++ Assets/Scripts/CarEngine.cs
@@ -1,30 +1,51 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class CarEngine : MonoBehaviour
 {
-    public float velocity = 10f;
-    public float rotationSpeed = 100f;
+    [Header("Movement")]
+    public float forwardSpeed = 70f;
+    public float lateralSpeed = 10f;
+    public float acceleration = 10f;
+    public float brakeForce = 10f;
+    
+    private Rigidbody rb;
+    private float currentSpeed = 10f;
+    private float verticalInput;
+    private float horizontalInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | 
+                        RigidbodyConstraints.FreezeRotationY |
+                        RigidbodyConstraints.FreezeRotationZ;
+    }
 
     void Update()
     {
-        // 1. INPUTS
-        float verticalInput = Input.GetAxis("Vertical");   // W / S
-        float horizontalInput = Input.GetAxis("Horizontal"); // A / D
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
 
-        // 2. DESPLAZAMIENTO
-        Vector3 movement = Vector3.forward * verticalInput * velocity * Time.deltaTime;
-        transform.Translate(movement, Space.Self);
-
-        // 3. ROTACIÓN
-        if (Mathf.Abs(verticalInput) > 0.1f)
+    void FixedUpdate()
+    {
+        if (verticalInput != 0)
         {
-            // DETECTAR DIRECCIÓN (Si es hacia delante o hacia atrás)
-            float direction = verticalInput > 0 ? 1 : -1;
-
-            // Multiplicamos el giro por 'direction'.
-            float turn = horizontalInput * rotationSpeed * Time.deltaTime * direction;
-            
-            transform.Rotate(0, turn, 0, Space.Self);
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 
+                                            forwardSpeed * verticalInput, 
+                                            acceleration * Time.fixedDeltaTime);
         }
+        else
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0, 
+                                            brakeForce * Time.fixedDeltaTime);
+        }
+
+        Vector3 forwardMovement = transform.forward * currentSpeed * Time.fixedDeltaTime;
+        Vector3 lateralMovement = transform.right * horizontalInput * lateralSpeed * Time.fixedDeltaTime;
+        
+        rb.MovePosition(rb.position + forwardMovement + lateralMovement);
     }
 }
